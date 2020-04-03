b268d47d6067c2cd66ec24ccd2ad77cc2328b087
Nikki Lin <nikkiylin@gmail.com>
2020-04-03 14:01:56 -0700

    Display the list of the legoCalendar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
 
+
+  const legoCalendar = [
+    {id: 1, title: "create a roller coaster"},
+    {id: 2, title: "build a new rocket"},
+    {is: 3, title: "build a new home"},
+    {id: 4, title: "build a new Star War movie set"},
+    {id: 5, title: "build a tallest tower"},
+    {id: 6, title: "build a new space ship"},
+    {id: 7, title: "build a coolest pick up truck"}
+    
+  ]
+
+const Home = () => {
+  return (
+    <div>
+      <h2>Home</h2>
+      {legoCalendar.map((item) => <p>Day {item.id} :  Title: {item.title}</p>)}
+      
+    </div>
+  )
+}
+
+const Login = () => {
+  return (
+    <div>
+      <h2>Login</h2>
+    </div>
+  )
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Lego-Points</h1>
+     <Router>
+       <div className="links">
+          <Link className="link" exact to="/">Home</Link>
+          <Link className="link" to="/Login">Login</Link>
+       </div>
+       <Route exact path="/" component={Home} />
+       <Route path="/login" component={Login} /> 
+     </Router>
     </div>
   );
 }
